2a239461dd7c0d1df786f60fa8a80d78eaf20f80
Marcelo Emanoel Bezerra Diniz <marceloemanoel@gmail.com>
2011-12-29 16:12:50 -0300

    correction on the configurations and dependencies closures and separating plugin convention file

--- src/main/groovy/br/com/smartcoders/migration/plugin/MigrationPlugin.groovy
+++ src/main/groovy/br/com/smartcoders/migration/plugin/MigrationPlugin.groovy
@@ -1,34 +1,28 @@
 package br.com.smartcoders.migration.plugin;
+
 import org.gradle.api.*
 
 import br.com.smartcoders.migration.tasks.InitTask
 
+
 class MigrationPlugin implements Plugin<Project> {
   
+  
   def void apply(Project project) {
+    def convention = new MigrationPluginConvention(project) 
+  
+    project.convention.plugins.migration = convention
     
     project.getRepositories().mavenCentral()
-    project.getConfigurations().add("migrations")
-    project.getDependencies().add("migrations", "org.mybatis:mybatis:3.0.6")
-    
-    project.convention.plugins.migration = new MigrationPluginConvention(project)
-
-    project.tasks.init = new InitTask()
-    
-    project.task('migrate') << {
-      println "do something!"
+    project.configurations {
+      migrations
     }
-  
+    project.dependencies {
+      "migrations"  "org.mybatis:mybatis:3.0.6" 
+    }
+    
+    project.task('init', type: InitTask) {
+      baseDir = new File(convention.baseDir)
+    }
   }
-}
-
-class MigrationPluginConvention {
-  
-  Project project;
-  def migrationBaseDir = "migrations"
-  
-  public MigrationPluginConvention(Project project) {
-    this.project = project
-  }  
-  
 }
