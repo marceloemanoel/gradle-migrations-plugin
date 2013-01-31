bccf3a3af895a6aa578647bb504687ce4b19f811
Mircea Pop <mircea.pop@epages.com>
2013-01-31 16:53:06 +0100

    down and status tasks.fix NullPointerException in UpTask.don't copy the jdbc driver if already available.

--- src/main/groovy/br/com/smartcoders/migration/plugin/MigrationPlugin.groovy
+++ src/main/groovy/br/com/smartcoders/migration/plugin/MigrationPlugin.groovy
@@ -1,50 +1,62 @@
 package br.com.smartcoders.migration.plugin;
 
 import org.gradle.api.*
-import org.gradle.api.artifacts.Configuration.State
+
 import br.com.smartcoders.migration.tasks.*
 
 class MigrationPlugin implements Plugin<Project> {
-  
-  def void apply(Project project) {
-    def convention = new MigrationPluginConvention(project) 
-  
-    project.convention.plugins.migration = convention
-    
-    project.getRepositories().mavenCentral()
-    project.configurations {
-      migrationDriver
-      migrations
-    }
 
-    project.dependencies {
-      "migrations"  "org.mybatis:mybatis:3.0.6" 
-    }
-    
-    project.task('migrateInit', type: InitTask) {
-      baseDir = project.file(convention.baseDir)
-	  environment = convention.environment
-	  force = convention.force
-    }
-    
-    project.task("migrateUp", type: UpTask) {
-      baseDir = project.file(convention.baseDir)
-	  environment = convention.environment
-	  force = convention.force
-	  if(project.hasProperty("stepCount")) {
-		  stepCounter = project.stepCount
-	  }
-    }
-	
-	project.task("migrateBootstrap", type: BootstrapTask) {
-      baseDir = project.file(convention.baseDir)
-	  environment = convention.environment
-	  force = convention.force
-    }
+    def void apply(Project project) {
+        def convention = new MigrationPluginConvention(project)
 
-	project.task("migrateDriver", type: DriverTask) {
-	  baseDir = project.file(convention.baseDir)
-	  environment = convention.environment
-	}
-  }
+        project.convention.plugins.migration = convention
+
+        project.getRepositories().mavenCentral()
+        project.configurations {
+            migrationDriver
+            migrations
+        }
+
+        project.dependencies { "migrations"  "org.mybatis:mybatis:3.0.6"  }
+
+        project.task('migrateInit', type: InitTask) {
+            baseDir = project.file(convention.baseDir)
+            environment = convention.environment
+            force = convention.force
+        }
+
+        project.task("migrateUp", type: UpTask, dependsOn: 'migrateDriver') {
+            baseDir = project.file(convention.baseDir)
+            environment = convention.environment
+            force = convention.force
+            if(project.hasProperty("steps")) {
+                steps = project.steps
+            }
+        }
+
+        project.task("migrateBootstrap", type: BootstrapTask, dependsOn: 'migrateDriver') {
+            baseDir = project.file(convention.baseDir)
+            environment = convention.environment
+            force = convention.force
+        }
+
+        project.task("migrateDriver", type: DriverTask) {
+            baseDir = project.file(convention.baseDir)
+        }
+
+        project.task("migrateStatus", type: StatusTask, dependsOn: 'migrateDriver') {
+            baseDir = project.file(convention.baseDir)
+            environment = convention.environment
+            force = convention.force
+        }
+
+        project.task("migrateDown", type: DownTask, dependsOn: 'migrateDriver') {
+            baseDir = project.file(convention.baseDir)
+            environment = convention.environment
+            force = convention.force
+            if(project.hasProperty("steps")) {
+                steps = project.steps
+            }
+        }
+    }
 }
--- src/main/groovy/br/com/smartcoders/migration/tasks/BootstrapTask.groovy
+++ src/main/groovy/br/com/smartcoders/migration/tasks/BootstrapTask.groovy
@@ -2,23 +2,36 @@ package br.com.smartcoders.migration.tasks;
 
 import org.apache.ibatis.migration.commands.BootstrapCommand
 import org.gradle.api.DefaultTask
-import org.gradle.api.artifacts.Configuration
+import org.gradle.api.Task
 import org.gradle.api.tasks.TaskAction
 
 class BootstrapTask extends DefaultTask {
-  
-  File baseDir
-  String environment
-  Boolean force
-  
-  public BootstrapTask(){
-    setDescription("Bootstrap migrations");
-    setGroup("Migration");
-  }
-  
-  @TaskAction
-  void bootstrap() {
-    def command = new BootstrapCommand(baseDir, environment, force)
-    command.execute()
-  }
+
+    File baseDir
+    String environment
+    Boolean force
+
+    public BootstrapTask(){
+        setDescription("Bootstrap migrations");
+        setGroup("Migration");
+    }
+
+    @TaskAction
+    void bootstrap() {
+        def command = new BootstrapCommand(baseDir, environment, force)
+        command.execute()
+    }
+
+    @Override
+    public Task doLast(Closure action) {
+        def clean = {
+            logger.info "cleaning the drivers folder..."
+            File driverFolder = new File(baseDir, "drivers")
+            if (driverFolder.exists()) {
+                File[] files = driverFolder.listFiles()
+                files.each { it.delete() }
+            }            
+        }
+        return super.doLast(clean << action);
+    }
 }
