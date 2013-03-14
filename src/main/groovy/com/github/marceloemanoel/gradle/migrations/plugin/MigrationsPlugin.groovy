ba295fa3200f58437867d94c823648d7d39dc417
Marcelo Emanoel Bezerra Diniz <marceloemanoel@gmail.com>
2013-03-14 01:04:25 -0300

    Add BootstrapTask to plugin

--- src/main/groovy/com/github/marceloemanoel/gradle/migrations/plugin/MigrationsPlugin.groovy
+++ src/main/groovy/com/github/marceloemanoel/gradle/migrations/plugin/MigrationsPlugin.groovy
@@ -3,6 +3,7 @@ package com.github.marceloemanoel.gradle.migrations.plugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
+import com.github.marceloemanoel.gradle.migrations.tasks.BootstrapTask
 import com.github.marceloemanoel.gradle.migrations.tasks.InitTask
 import com.github.marceloemanoel.gradle.migrations.tasks.UpTask
 
@@ -12,23 +13,35 @@ class MigrationsPlugin implements Plugin<Project> {
     public void apply(Project project) {
         project.extensions.create("migrations", MigrationPluginExtension)
         def extensionPoint = project.migrations
-        
+
         project.getRepositories().mavenCentral()
         project.configurations {
             migrationDriver
             migrations
         }
-        project.dependencies { "migrations"  "org.mybatis:mybatis:3.0.6" }
+        project.dependencies {
+            "migrations"  "org.mybatis:mybatis:3.0.6"
+        }
 
         project.task('migrateInit', type: InitTask) {
             baseDir = project.file(extensionPoint.baseDir)
+            environment = extensionPoint.environment
             force = extensionPoint.force
         }
 
         project.task("migrateUp", type: UpTask) {
             baseDir = project.file(extensionPoint.baseDir)
-            if(project.hasProperty("stepCount"))
-                stepCounter = project.stepCount
+            environment = extensionPoint.environment
+            force = extensionPoint.force
+            if(project.hasProperty("steps")) {
+                steps = project.steps
+            }
+        }
+        
+        project.task("migrateBootstrap", type: BootstrapTask) {
+            baseDir = project.file(extensionPoint.baseDir)
+            environment = extensionPoint.environment
+            force = extensionPoint.force
         }
     }
 }
--- src/test/groovy/com/github/marceloemanoel/gradle/migrations/plugin/MigrationsPluginTest.groovy
+++ src/test/groovy/com/github/marceloemanoel/gradle/migrations/plugin/MigrationsPluginTest.groovy
@@ -8,30 +8,38 @@ import org.gradle.testfixtures.ProjectBuilder
 import org.junit.Before
 import org.junit.Test
 
+import com.github.marceloemanoel.gradle.migrations.tasks.BootstrapTask
 import com.github.marceloemanoel.gradle.migrations.tasks.InitTask
-import com.github.marceloemanoel.gradle.migrations.tasks.UpTask;
+import com.github.marceloemanoel.gradle.migrations.tasks.UpTask
 
 class MigrationsPluginTest {
-    
+
     def Project project;
-    
+
     @Before
     public void setUp(){
         project = ProjectBuilder.builder().build()
         project.apply plugin: MigrationsPlugin
     }
-    
+
     @Test
     void afterApplyPluginProjectShouldHaveMigrateInitTask(){
-        Task initTask = project.tasks.migrateInit
-        assertNotNull initTask 
-        assertTrue InitTask.class.isInstance(initTask)
+        Task task = project.tasks.migrateInit
+        assertNotNull task
+        assertTrue InitTask.class.isInstance(task)
     }
-    
+
     @Test
     void afterApplyPluginProjectShouldHaveMigrateUpTask(){
-        Task upTask = project.tasks.migrateUp
-        assertNotNull upTask
-        assertTrue UpTask.class.isInstance(upTask)
+        Task task = project.tasks.migrateUp
+        assertNotNull task
+        assertTrue UpTask.class.isInstance(task)
+    }
+
+    @Test
+    void afterApplyPluginProjectShouldHaveMigrateBootstrapTask(){
+        Task task = project.tasks.migrateBootstrap
+        assertNotNull task
+        assertTrue BootstrapTask.class.isInstance(task)
     }
 }
