a072f8610cd701e9970cc9e8e9c130385d0ab99c
Marcelo Emanoel Bezerra Diniz <marceloemanoel@gmail.com>
2013-03-14 00:18:38 -0300

    Refactor InitTask

--- src/main/groovy/com/github/marceloemanoel/gradle/migrations/tasks/InitTask.groovy
+++ src/main/groovy/com/github/marceloemanoel/gradle/migrations/tasks/InitTask.groovy
@@ -2,46 +2,39 @@ package com.github.marceloemanoel.gradle.migrations.tasks
 
 import org.apache.ibatis.migration.commands.InitializeCommand
 import org.gradle.api.DefaultTask
-import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.artifacts.Configuration
 import org.gradle.api.tasks.TaskAction
 
 class InitTask extends DefaultTask {
-  
-  File baseDir
-  String environment
-  
-  public InitTask(){
-    setDescription("Create migrations structure");
-    setGroup("Migration");
-  }
-  
-  @TaskAction
-  void createDirectories() {
-    logger.info "Creating directory."
-    
-    if(!baseDir.exists())
-      baseDir.mkdirs();
-    
-    def command = new InitializeCommand(baseDir, "development", true)
-    command.execute()
-    
-    File driversDir = new File(baseDir, "drivers");
-    driversDir.deleteDir();
-    
-    File environmentsDir = new File(baseDir, "environments")
-    
-    def props = new Properties()
-    def devEnv = new File(environmentsDir,"development.properties")
-    devEnv.withInputStream { stream ->
-      props.load(stream)
+
+    File baseDir
+    String environment
+    Boolean force
+
+    public InitTask(){
+        setDescription("Create migrations structure")
+        setGroup("Migration")
     }
-    
-    props.driver_path = getProject().getConfigurations().migrationDriver.singleFile.parent
-    
-    devEnv.withOutputStream { out ->
-      props.store(out, "")
+
+    @TaskAction
+    void createDirectories() {
+        if(baseDir.exists()) {
+            if(force){
+                logger.info "Deleting directory."
+                baseDir.deleteDir()
+            }
+            else{
+                logger.info "migrations dir already exists: " + baseDir.getAbsolutePath()
+                return
+            }
+        } 
+        else {
+            logger.info "Creating directory."
+            baseDir.mkdirs()
+        }
+        
+        new InitializeCommand(baseDir, environment, force).execute()
+        
+        logger.info "Directory created at '${baseDir.absolutePath}'."
     }
-    
-    logger.info "Directory created at '${baseDir.absolutePath}'."
-  }
 }
