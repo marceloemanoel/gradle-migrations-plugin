300e879da88e432458825a138b603eb719faa041
Marcelo Emanoel Bezerra Diniz <marceloemanoel@gmail.com>
2013-03-14 01:11:10 -0300

    Update package of DownTask

--- src/main/groovy/com/github/marceloemanoel/gradle/migrations/tasks/DownTask.groovy
+++ src/main/groovy/com/github/marceloemanoel/gradle/migrations/tasks/DownTask.groovy
@@ -1,20 +1,21 @@
-package br.com.smartcoders.migration.tasks
+package com.github.marceloemanoel.gradle.migrations.tasks
 
 import org.apache.ibatis.migration.commands.DownCommand
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
+
+import com.github.marceloemanoel.gradle.migrations.helper.CommandHelper
 
 class DownTask extends DefaultTask {
 
     File baseDir
     String environment
     Boolean force
-
-    String steps = ""
+    String steps
 
     public DownTask(){
-        setDescription("Executes migration down command.Configurable params: steps");
-        setGroup("Migration");
+        setDescription("Executes migration down command.Configurable params: steps")
+        setGroup("Migration")
     }
 
     @TaskAction
