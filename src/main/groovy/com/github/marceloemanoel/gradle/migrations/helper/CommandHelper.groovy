5a2bb791cee3bce5d0d7361fce96f874928d0950
Marcelo Emanoel Bezerra Diniz <marceloemanoel@gmail.com>
2013-03-14 00:48:30 -0300

    Update package

--- src/main/groovy/com/github/marceloemanoel/gradle/migrations/helper/CommandHelper.groovy
+++ src/main/groovy/com/github/marceloemanoel/gradle/migrations/helper/CommandHelper.groovy
@@ -1,10 +1,8 @@
-package br.com.smartcoders.migration.tasks;
+package com.github.marceloemanoel.gradle.migrations.helper
 
 import org.apache.ibatis.migration.commands.BaseCommand
 import org.gradle.api.DefaultTask
-import org.gradle.api.Project;
-
-
+import org.gradle.api.Project
 
 final class CommandHelper {
 
