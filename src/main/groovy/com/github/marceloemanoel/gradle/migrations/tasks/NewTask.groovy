7b72c241f92fc994db906ab1b8317701e25cc30e
Marcelo Emanoel Bezerra Diniz <marceloemanoel@gmail.com>
2013-03-14 01:37:12 -0300

    Update package of NewTask

--- src/main/groovy/com/github/marceloemanoel/gradle/migrations/tasks/NewTask.groovy
+++ src/main/groovy/com/github/marceloemanoel/gradle/migrations/tasks/NewTask.groovy
@@ -1,19 +1,22 @@
-package br.com.smartcoders.migration.tasks
+package com.github.marceloemanoel.gradle.migrations.tasks
 
 import org.apache.ibatis.migration.commands.NewCommand
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 import org.gradle.cli.CommandLineArgumentException
 
+import com.github.marceloemanoel.gradle.migrations.helper.CommandHelper;
+
 class NewTask extends DefaultTask {
+    
     File baseDir
     String environment
     String template
-    String fileDescription = ""
+    String fileDescription
     Boolean force
 
     public NewTask(){
-        setDescription("Execute migrations new command.Configurable params: des, template");
+        setDescription("Execute migrations new command.Configurable params: description, template")
         setGroup("Migration");
     }
 
@@ -21,7 +24,7 @@ class NewTask extends DefaultTask {
     def executeMigrations() {
         if (fileDescription.isEmpty()) {
             throw new CommandLineArgumentException("Please provide a description for the new file:" +
-            " \nUsage: gradle migrateNew -Pdes=\"your description here\" [-Ptemplate=\"template path\"]")
+            " \nUsage: gradle migrateNew -Pdescription=\"your description here\" [-Ptemplate=\"template path\"]")
         }
         def command = new NewCommand(baseDir, environment, template, force)
         CommandHelper.updateDriverClassLoader(project, command)
