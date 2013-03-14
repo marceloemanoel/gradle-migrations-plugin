9e83cc3a13982ff151362bf1f5af626ab6c1d362
Marcelo Emanoel Bezerra Diniz <marceloemanoel@gmail.com>
2013-03-14 00:55:38 -0300

    Update package of BootstrapTask

--- src/main/groovy/com/github/marceloemanoel/gradle/migrations/tasks/BootstrapTask.groovy
+++ src/main/groovy/com/github/marceloemanoel/gradle/migrations/tasks/BootstrapTask.groovy
@@ -1,8 +1,10 @@
-package br.com.smartcoders.migration.tasks;
+package com.github.marceloemanoel.gradle.migrations.tasks
 
 import org.apache.ibatis.migration.commands.BootstrapCommand
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
+
+import com.github.marceloemanoel.gradle.migrations.helper.CommandHelper;
 
 class BootstrapTask extends DefaultTask {
 
