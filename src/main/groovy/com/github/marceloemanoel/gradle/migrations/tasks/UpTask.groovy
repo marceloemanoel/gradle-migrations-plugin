d708421ac19a8a658e4fc1b7d266835d11391360
Marcelo Emanoel Bezerra Diniz <marceloemanoel@gmail.com>
2013-03-14 00:19:14 -0300

    Refactor UpTask

--- src/main/groovy/com/github/marceloemanoel/gradle/migrations/tasks/UpTask.groovy
+++ src/main/groovy/com/github/marceloemanoel/gradle/migrations/tasks/UpTask.groovy
@@ -1,24 +1,23 @@
 package com.github.marceloemanoel.gradle.migrations.tasks
 
-import org.apache.ibatis.migration.commands.UpCommand;
+import org.apache.ibatis.migration.commands.UpCommand
 import org.gradle.api.DefaultTask
-import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.tasks.TaskAction
 
 class UpTask extends DefaultTask {
-  
-  File baseDir
-  String environment
-  String stepCounter
-  
-  public UpTask(){
-    setDescription("Execute migrations");
-    setGroup("Migration");
-  }
-  
-  @TaskAction
-  def executeMigrations() {
-    def command = new UpCommand(baseDir, "development", true)
-    command.execute(stepCounter)
-  }
-  
+
+    File baseDir
+    String environment
+    String stepCounter
+    Boolean force
+
+    public UpTask(){
+        setDescription("Execute migrations")
+        setGroup("Migration")
+    }
+
+    @TaskAction
+    def executeMigrations() {
+        new UpCommand(baseDir, environment, force).execute(stepCounter)
+    }
 }
