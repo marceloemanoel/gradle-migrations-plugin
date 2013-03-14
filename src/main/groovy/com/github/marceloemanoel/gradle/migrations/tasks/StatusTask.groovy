efc4432d35850b1eb90ff53385ebaae583d2fb83
Marcelo Emanoel Bezerra Diniz <marceloemanoel@gmail.com>
2013-03-14 01:44:35 -0300

    Update package of StatusTask

--- src/main/groovy/com/github/marceloemanoel/gradle/migrations/tasks/StatusTask.groovy
+++ src/main/groovy/com/github/marceloemanoel/gradle/migrations/tasks/StatusTask.groovy
@@ -1,18 +1,21 @@
-package br.com.smartcoders.migration.tasks
+package com.github.marceloemanoel.gradle.migrations.tasks
 
 import org.apache.ibatis.migration.commands.StatusCommand
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
+import com.github.marceloemanoel.gradle.migrations.helper.CommandHelper
+
 
 class StatusTask extends DefaultTask {
+    
     File baseDir
     String environment
     Boolean force
 
     public StatusTask() {
-        setDescription("Shows migrations status");
-        setGroup("Migration");
+        setDescription("Shows migrations status")
+        setGroup("Migration")
     }
 
     @TaskAction
