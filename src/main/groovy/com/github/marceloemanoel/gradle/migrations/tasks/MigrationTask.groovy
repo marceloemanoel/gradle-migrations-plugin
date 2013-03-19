ceb1d40b8d042757d76c0c5c1629025106f0a2c4
Marcelo Emanoel Bezerra Diniz <marceloemanoel@gmail.com>
2013-03-19 16:20:01 -0300

    Add shorter parameters

--- src/main/groovy/com/github/marceloemanoel/gradle/migrations/tasks/MigrationTask.groovy
+++ src/main/groovy/com/github/marceloemanoel/gradle/migrations/tasks/MigrationTask.groovy
@@ -11,14 +11,24 @@ class MigrationTask extends DefaultTask {
     }
     
     def File getBaseDir(){
+        if(project.hasProperty("b"))
+            return project.file(project.b)
         project.file(project.migrations.baseDir)
     }
     
     def String getEnvironment() {
+        if(project.hasProperty("e"))
+            return project.e
+        if(project.hasProperty("env"))
+            return project.env
+        if(project.hasProperty("environment"))
+            return project.environment
         project.migrations.environment
     }
     
     def Boolean getForce(){
+        if(project.hasProperty("f"))
+            return project.f
         project.migrations.force
     }
 }
