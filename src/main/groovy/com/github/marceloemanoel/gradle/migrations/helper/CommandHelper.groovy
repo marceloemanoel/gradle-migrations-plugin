24b91a5648b6f9f349d4312c3989f9da422a8ed4
Marcelo Emanoel Bezerra Diniz <marceloemanoel@gmail.com>
2013-06-24 01:21:01 -0300

    Revert "Add back migrationsDriver configuration for now"
    This reverts commit 30f56b3f4b056f5938dcf42923b14a65192e2f0b.

--- src/main/groovy/com/github/marceloemanoel/gradle/migrations/helper/CommandHelper.groovy
+++ src/main/groovy/com/github/marceloemanoel/gradle/migrations/helper/CommandHelper.groovy
@@ -3,40 +3,34 @@ package com.github.marceloemanoel.gradle.migrations.helper
 import org.apache.ibatis.migration.commands.BaseCommand
 import org.gradle.api.DefaultTask
 import org.gradle.api.Project
-import org.gradle.api.artifacts.Configuration;
 
 final class CommandHelper {
-
+    
     private Project project;
-    private Configuration configuration;
-
+    
     public CommandHelper(Project project) {
         this.project = project;
     }
-
+    
+    public updateDriverClassLoader(BaseCommand command) {
+        def dependencies = getDependencies();
+        command.setDriverClassLoader(new URLClassLoader(dependencies.toArray(new URL[dependencies.size])))
+    }
+    
+    def getDependencies() {
+        configuration().files.collect {
+            it.toURI().toURL()
+        }
+    }
+    
     def configuration() {
-        def driverConfig = project.getConfigurations().findByName("migrationsDriver");
-        if(driverConfig && !driverConfig.empty) {
-            project.logger.warn "The 'migrationDriver' configuration has been deprecated and is scheduled to be removed in the next release. It is now replaced by the 'compile' configuration."
-            driverConfig;
+        def migrationDriver = project.getConfigurations().findByName("migrationDriver");
+        if(migrationDriver != null) {
+            project.logger.warn("The 'migrationDriver' configuration has been deprecated and is scheduled to be removed in the next release. It is now replaced by the 'compile' configuration.")
+            migrationDriver;
         }
         else {
             project.getConfigurations().findByName("compile");
         }
     }
-
-    def ClassLoader getDriverClassLoader() {
-        if(!configuration) {
-            configuration = configuration();
-        }
-        def dependencies = getDependencies()
-        new URLClassLoader(dependencies.toArray(new URL[dependencies.size]))
-    }
-
-    def getDependencies() {
-        configuration.files.collect {
-            it.toURI().toURL()
-        }
-    }
-
 }
